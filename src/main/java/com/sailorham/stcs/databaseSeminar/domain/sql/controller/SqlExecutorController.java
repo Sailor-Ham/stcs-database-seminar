9a921ff831b6d66bfb86396d2e7be7b8a2d400db
Sailor-Ham <sangja234@gmail.com>
2026-01-12 22:52:38 +0900

    feat(db-schema): Database Schema 조회 로직 작성 및 API Endpoint 매핑

--- src/main/java/com/sailorham/stcs/databaseSeminar/domain/sql/controller/SqlExecutorController.java
+++ src/main/java/com/sailorham/stcs/databaseSeminar/domain/sql/controller/SqlExecutorController.java
@@ -3,9 +3,12 @@ package com.sailorham.stcs.databaseSeminar.domain.sql.controller;
 import com.sailorham.stcs.databaseSeminar.common.response.ApiResponse;
 import com.sailorham.stcs.databaseSeminar.domain.sql.dto.SqlRequest;
 import com.sailorham.stcs.databaseSeminar.domain.sql.dto.SqlResponse;
+import com.sailorham.stcs.databaseSeminar.domain.sql.dto.TableSchemaResponse;
 import com.sailorham.stcs.databaseSeminar.domain.sql.service.SqlExecutorService;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +24,10 @@ public class SqlExecutorController {
     @PostMapping("/execute")
     public ApiResponse<SqlResponse> execute(@RequestBody @Valid SqlRequest request) {
         return ApiResponse.success(sqlExecutorService.execute(request));
+    }
+
+    @GetMapping("/schema")
+    public ApiResponse<List<TableSchemaResponse>> getDatabaseSchema() {
+        return ApiResponse.success(sqlExecutorService.getDatabaseSchema());
     }
 }
